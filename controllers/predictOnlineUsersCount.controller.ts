05604fa772dcdde7299e76a118bddfce9fd20565
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-10-09 17:33:22 +0300

    LSO-31 Created endpoint for predicting online users count

---
+++ controllers/predictOnlineUsersCount.controller.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from 'express';
+
+export const predictOnlineUsersCount = async (req: Request, res: Response) => {};
+
+export const predictNumberOfUsers = () => {};
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { getHistoricalDataForAll } from './controllers/getHistoricalDataForAll.controller';
 import { getHistoricalDataForUser } from './controllers/getHistoricalDataForUser.controller';
+import { predictOnlineUsersCount } from './controllers/predictOnlineUsersCount.controller';
 
 const app = express();
 app.use(express.json());
@@ -10,3 +11,5 @@ app.listen(3001, () => console.log('Server working'));
 app.get('/api/stats/users', getHistoricalDataForAll);
 
 app.get('/api/stats/user', getHistoricalDataForUser);
+
+app.get('/api/predictions/users', predictOnlineUsersCount);
