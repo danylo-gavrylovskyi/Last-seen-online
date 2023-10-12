df32bec4754e4e47c61acda48e5d7af44feb9764
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-10-12 18:50:30 +0300

    LSO-42 Made function to calculate total online time

--- controllers/getTotalOnlineTime.controller.ts
+++ controllers/getTotalOnlineTime.controller.ts
@@ -9,4 +9,26 @@ export const getTotalOnlineTime = () => {};
 export const getUserTotalOnlineTime = (
   data: LastSeenUser[],
   userId: string,
-): TotalOnlineTimeRes => {};
+): TotalOnlineTimeRes => {
+  const userData = data.filter((entry) => entry.userId === userId);
+
+  if (userData.length === 0) {
+    return { totalTime: null };
+  }
+
+  let start_time = null;
+  let total_duration = 0;
+
+  for (const row of userData) {
+    if (row.isOnline && start_time === null && row.lastSeenDate) {
+      start_time = new Date(row.lastSeenDate);
+    } else if (!row.isOnline && start_time !== null && row.lastSeenDate) {
+      const end_time = new Date(row.lastSeenDate);
+      const timeDifferenceInSeconds = (end_time.getDate() - start_time.getDate()) / 1000;
+      total_duration += timeDifferenceInSeconds;
+      start_time = null;
+    }
+  }
+
+  return { totalTime: total_duration };
+};
