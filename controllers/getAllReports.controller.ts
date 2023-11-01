c4b4640f64fe3e7cd20042c4df60172af93cafcf
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-11-01 03:09:58 +0200

    LSO-61 implemented getting all reports feature

--- controllers/getAllReports.controller.ts
+++ controllers/getAllReports.controller.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
-import { users } from "../app";
+import { reports } from "../app";
 import { UserReport } from "../types/report.interface";
 
 export const getAllReports = async (req: Request, res: Response) => {
-	const usersData = users.getData();
-
-	const response = getExistedReports();
+	const response = getExistedReports(reports);
 
 	return res.status(200).json(response);
 };
 
-export const getExistedReports = (): UserReport[] => {};
+export const getExistedReports = (reports: UserReport[]): UserReport[] => {
+	return reports;
+};
