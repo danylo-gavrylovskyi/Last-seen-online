0abf3238ba1c05b3fdb57be5a469939947ec5e7f
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-11-02 09:21:51 +0200

    LSO-63 created endpoint for list of users

---
+++ controllers/getListOfUsers.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { users } from '../app';
+
+interface UserData {
+  username: string;
+  userId: string;
+  firstSeen: Date;
+}
+
+export const getListOfUsers = async (req: Request, res: Response) => {
+  const usersData = users.getData();
+
+  const result = getUserList();
+
+  return res.status(200).json(result);
+};
+
+export const getUserList = (): UserData => {};
